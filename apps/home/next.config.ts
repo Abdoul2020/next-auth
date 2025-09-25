4603ba567616bdaeaaae16d693c4a1fc9ef2e817
AbdoulKo <44230084+Abdoul2020@users.noreply.github.com>
2025-09-25 09:39:04 +0300

    Update next.config.ts

--- apps/home/next.config.ts
+++ apps/home/next.config.ts
@@ -4,6 +4,19 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
+
+  async rewrites() {
+    return [
+      {
+        source: 'cart/en/cart/:path*',
+        destination: 'https://next-auth-8sjz.vercel.app/cart/:path*',
+      },
+      {
+        source: 'cart/tr/cart/:path*',
+        destination: 'https://next-auth-8sjz.vercel.app/cart/:path*',
+      },
+    ];
+  },
   
 };
 
