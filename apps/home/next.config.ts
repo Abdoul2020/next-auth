8d0097759b56ed3113738ef6b70ae4c4289c8215
AbdoulKo <44230084+Abdoul2020@users.noreply.github.com>
2025-09-25 08:54:21 +0300

    Update next.config.ts

--- apps/home/next.config.ts
+++ apps/home/next.config.ts
@@ -1,25 +1,21 @@
-import type { NextConfig } from "next";
-
-import createNextIntlPlugin from "next-intl/plugin";
+// next.config.js
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {
-  /* config options here */
-    allowedOrigins: ["https://next-auth-h9c5.vercel.app/"],
-
+const nextConfig = {
   async rewrites() {
     return [
       {
         source: '/en/cart/:path*',
-        destination: 'https://next-auth-8sjz.vercel.app/cart/:path*'
+        destination: 'https://next-auth-8sjz.vercel.app/cart/:path*',
       },
       {
         source: '/tr/cart/:path*',
-        destination: 'https://next-auth-8sjz.vercel.app/cart/:path*'
-      }
+        destination: 'https://next-auth-8sjz.vercel.app/cart/:path*',
+      },
     ];
-  }
+  },
 };
 
 export default withNextIntl(nextConfig);
