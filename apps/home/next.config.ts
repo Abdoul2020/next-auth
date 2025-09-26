61fb925800807832945100fac414288d59dd6728
AbdoulKo <44230084+Abdoul2020@users.noreply.github.com>
2025-09-26 10:54:09 +0300

    Update next.config.ts

--- apps/home/next.config.ts
+++ apps/home/next.config.ts
@@ -7,21 +7,17 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
-  // If you have a basePath, take that into account; omitted here.
   async rewrites() {
     return [
-      // Exact root path for English cart page
       {
         source: "/en/cart",
         destination: "https://next-auth-8sjz.vercel.app/cart/en/cart",
       },
-      // Any subpaths under /en/cart/*
       {
         source: "/en/cart/:path*",
         destination: "https://next-auth-8sjz.vercel.app/cart/en/cart/:path*",
       },
 
-      // Turkish locale (maps to the same cart page on the cart app)
       {
         source: "/tr/cart",
         destination: "https://next-auth-8sjz.vercel.app/cart/en/cart",
